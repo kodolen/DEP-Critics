3210ef6cc388584e8bc9518f21da68a0291f29cd
Lennart <lennartjvw@icloud.com>
2019-10-23 12:40:42 +0200

    Added access to premium user

--- app/Http/Controllers/CriticController.php
+++ app/Http/Controllers/CriticController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\CriticRequest;
 use App\Player;
 use App\Critic;
+use App\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
@@ -18,7 +19,13 @@ class CriticController extends Controller
     public function store(CriticRequest $id)
     {
         $player = Player::findOrFail($id);
-//        $number = $player[0]->id;
+        $user = Auth::user();
+
+        if($user->hasRole("user") && count($user->critics) >= 2){
+            $user->roles()->sync(2);
+            $user->save();
+        }
+
 
         Critic::create([
             'critic' => $id->critic,
